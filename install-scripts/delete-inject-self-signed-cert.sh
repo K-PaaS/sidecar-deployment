632336191283f6094f3422f61f56f52f677e93d5
okpc579 <ski579579@naver.com>
2021-12-22 08:07:24 +0000

    uaa version update

---
+++ install-scripts/delete-inject-self-signed-cert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source variables.yml
+
+kapp delete -a cert-injection-webhook
+
--- install-scripts/deploy-inject-self-signed-cert.sh
+++ install-scripts/deploy-inject-self-signed-cert.sh
@@ -2,11 +2,11 @@
 source variables.yml
 
 ################################################################################################################
-################################# inject-self-signed-certificate.sh 활용 가이드 #################################
+#################################     inject-self-signed-cert 활용 가이드      #################################
 ################################################################################################################
 # Private Repository (e.g. Harbor, distribution....) 사용 시 HTTPS가 지원되야 PaaS-TA Sidecar 설치가 가능하다.
 # HTTPS 설정 시 자체 서명된 인증서를 사용하려면 Private Repository를 사용하는 POD에 HTTPS 설정 시 사용한 인증서를 저장해야 한다.
-# inject-self-signed-certificate.sh에서 사용되는 cert-injection-webhook(https://github.com/vmware-tanzu/cert-injection-webhook)는 
+# deploy-inject-self-signed-cert.sh에서 사용되는 cert-injection-webhook(https://github.com/vmware-tanzu/cert-injection-webhook)는 
 # labels 또는 annotations을 설정하여 해당 labels 또는 annotations을 가진 POD가 배포될 시 컨테이너 내부에 인증서를 삽입한다.
 ################################################################################################################
 # 사용 방법
@@ -15,11 +15,10 @@ source variables.yml
 # 2-1. is_self_signed_certificate=true
 # 2-2. app_registry_cert_path=support-files/private-repository.ca
 # 3. app_registry_cert_path에 위치한 파일에 Private Repository에 사용된 인증서 CA를 넣는다.
-# 4. inject-self-signed-certificate.sh를 실행한다.
+# 4. deploy-inject-self-signed-cert.sh를 실행한다.
 # 5. Sidecar 설치 과정에 따라 2.generate-values.sh, 3.rendering-values.sh 스크립트를 실행한다.
 # 6. 4.deploy-sidecar.sh 스크립트를 실행하여 Sidecar를 설치한다.
 ################################################################################################################
-
 
 
 
