a0464217e71af80f05371aed3e55835d9b52ed90
Luna-irevit <107905603+Luna-irevit@users.noreply.github.com>
2023-12-07 14:00:11 +0900

    Update binding-admin.sh
    Signed-off-by: Luna-irevit <107905603+Luna-irevit@users.noreply.github.com>

--- install-scripts/support-files/user/binding-admin.sh
+++ install-scripts/support-files/user/binding-admin.sh
@@ -38,7 +38,7 @@ EOF
   if [[ $1 == ua ]]; then
     rolebinding_name="admin-${1}-${2}" yq e '.metadata.name = env(rolebinding_name)' rolebinding-template/rolebinding-admin.yaml | username="${2}" yq e '.subjects[0].name = env(username)'  | yq e '.subjects[0].kind = "User"'  | yq e 'del(.subjects[0].namespace)' | kubectl apply -f -
   elif [[ $1 == sa ]]; then
-    rolebinding_name="admin-${1}-${2}" yq e  '.metadata.name = env(rolebinding_name)' rolebinding-template/rolebinding-admin.yaml | username="${2}" yq e '.subjects[0].name = env(username)' | sa_namespace="${sa_namespace}" yq e '.subjects[0].namespace = env(sa_namespace)' | kubectl apply -f -
+    rolebinding_name="admin-${1}-${2}" yq e  '.metadata.name = env(rolebinding_name)' rolebinding-template/rolebinding-admin.yaml | username="${2}" yq e '.subjects[0].name = env(username)' | sa_namespace="${sa_namespace}" yq e '.subjects[0].namespace = env(sa_namespace)' | sa_namespace="${sa_namespace}" yq e '.metadata.namespace = env(sa_namespace)' | kubectl apply -f -
   fi
 }
 
